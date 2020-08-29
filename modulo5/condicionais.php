751aa360407b53c2687cf0ae6eb048cd356a5018
2020nani <her_almeida23@hotmail.com>
2020-08-29 09:19:47 -0300

    constantes,calculos matematicos ,condicionais e operadores logicos

---
+++ modulo5/condicionais.php
@@ -0,0 +1,24 @@
+<?php
+if(9 % 3 == 0){
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+ Aqui e meu site
+</body>
+</html>
+<?php
+
+}else{
+ 
+?>
+
+<?php
+   echo "Pagina nao pode ser exibida";
+}
+?>
--- modulo5/ola.php
+++ modulo5/ola.php
@@ -5,6 +5,30 @@ echo "Developer </br>";
 //declarando variavel
 $nome = "Guilherme </br>";
 echo $nome;
-$nome = 'Joao';
+$nome = 'Joao </br>';
 echo $nome;
+//concatenar msgem com variavel
+echo 'Meu nome e '.$nome;
+//concatenar msgem com variavel usando ""
+echo "Meu nome e $nome";
+//arrays
+$nomes = ['Hernani','marcelo','marcos'];
+echo 'Array posicao 0 = ' .$nomes[0];
+echo 'Array posicao 1 = ' .$nomes[1];
+$informacao['nome'] = 'hernani';
+$informacao['cidade'] = 'botucatu';
+echo '<br/>';
+echo $informacao['nome'];
+echo '<br/>';
+echo $informacao['cidade'];
+echo '<br/>';
+//condicionais
+$number = 5;
+if($number % 9 == 0){
+    echo 'O numero e par';
+}else{
+    echo 'O numero nao e par';
+}
+
 ?>
+
