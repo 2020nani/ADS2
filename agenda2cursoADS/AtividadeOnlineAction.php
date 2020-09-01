6231c14d4dbee4a65bc0ba3e441b989653fcd7a2
2020nani <her_almeida23@hotmail.com>
2020-08-31 23:40:26 -0300

    exercicio 2 curso ADS(formularios get e post) concluido

--- agenda2cursoADS/AtividadeOnlineAction.php
+++ agenda2cursoADS/AtividadeOnlineAction.php
@@ -1,2 +1,23 @@
 <?php
+ 
+    $nome = $_POST['txtNome'];
+    $sobrenome = $_POST['txtSobrenome'];
+    $email = $_POST['txtEmail'];
+    $formacao = $_POST['txtFormacao'];
+    $emprego = $_POST['txtEmprego'];
+    echo '<div 
+    style="background-color:teal; 
+    width:35vw;
+    margin-left:31vw;
+    margin-top:30vh;
+    padding:1px 4px 4px;
+    color:white;
+     "><h1>Cadastro Confirmado com Sucesso</h1><div/>';
+    echo 'Nome: ' .$nome .'<br/>'; 
+    echo 'Sobrenome: ' .$sobrenome .'<br/>';
+    echo 'Email: ' .$email .'<br/>';
+    echo 'Formacao: ' .$formacao .'<br/>';
+    echo 'Descrição do ultimo emprego: ' .$emprego .'<br/>';
+    
+ 
 ?>
